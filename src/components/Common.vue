6b91d2b5e9e02a4e4f2eaf31a6fda36d8bd1c415
xilongtaoPC <xilongtao@sporte.cn>
2017-10-31 16:33:21 +0800

    update

--- src/components/Common.vue
+++ src/components/Common.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container-fluid">
         <my-header home-active="true"></my-header>
-            <transition name="s">
-                <router-view />
-            </transition>
+            
+            <router-view />
+            
         <empty ></empty>
     </div>
 </template>
